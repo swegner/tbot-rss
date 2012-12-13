0ce2770ffdd76ee36eb164d5e9b47903b7e29772
Scott Wegner <swegner2@live.com>
2012-12-13 07:20:48 -0800

    Return a syndication feed

--- TbotRssService/RssController.cs
+++ TbotRssService/RssController.cs
@@ -1,12 +1,21 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.ServiceModel.Syndication;
+using System.Web.Http;
 
 namespace TbotRssService
 {
     public class RssController : ApiController
     {
-         public string Get()
+         public SyndicationFeed Get()
          {
-             return "my feed";
+             SyndicationFeed feed = new SyndicationFeed("title", "description", new Uri("http://alternativeLink"), "id",
+                                                        new DateTimeOffset(DateTime.Now));
+             feed.Items = new[]
+                 {
+                     new SyndicationItem("item title", "content", new Uri("http://alternativeLink/content"), "contentId",
+                                         new DateTimeOffset(DateTime.Now)),
+                 };
+             return feed;
          }
     }
 }
