5b5f900e54ac920fd215f97b8236127441633530
Scott Wegner <swegner2@live.com>
2012-12-14 17:37:54 -0800

    Minor cleanup

--- TbotRssService/Properties/AssemblyInfo.cs
+++ TbotRssService/Properties/AssemblyInfo.cs
@@ -1,23 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TbotRssService")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TbotRssService")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1422a468-9483-48f5-ac9c-6e46103e9054")]
@@ -31,5 +20,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*.*")]
+[assembly: AssemblyFileVersion("1.0.*.*")]
---
+++ TbotRssService/SyndicationFeedResult.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.ServiceModel.Syndication;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace TbotRssService
+{
+    public class SyndicationFeedResult : ContentResult
+    {
+        public SyndicationFeedResult(SyndicationFeed feed)
+        {
+            this.ContentType = "application/rss+xml";
+
+            using (TextWriter tWriter = new StringWriter())
+            {
+                using (XmlTextWriter writer = new XmlTextWriter(tWriter))
+                {
+                    feed.SaveAsRss20(writer);
+                }
+
+                this.Content = tWriter.ToString();
+            }
+        }
+    }
+}
