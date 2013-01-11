9187d366bc8dcc719ee36ca3b60b373fa89a3ea4
Scott Wegner <swegner2@live.com>
2013-01-11 13:16:55 -0800

    Add subtitle

---
+++ TbotRssService/Transforms/AddSubtitle.cs
@@ -0,0 +1,12 @@
+﻿namespace TbotRssService.Transforms
+{
+    using System.ServiceModel.Syndication;
+
+    public class AddSubtitle : ISyndicationFeedVisitor
+    {
+        public void TransformFeed(SyndicationFeed feed, SyndicationVisitorContext context)
+        {
+            feed.ElementExtensions.Add("subtitle", Constants.ItunesNS.NamespaceName, "Technology Business Analysis - Going beyond the headlines");
+        }
+    }
+}
