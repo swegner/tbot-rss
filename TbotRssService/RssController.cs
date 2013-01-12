78e4e86a4a926503ec546c6d396d2b010594d5fb
Scott Wegner <swegner2@live.com>
2013-01-11 16:53:27 -0800

    Add output caching. Fixes #2

--- TbotRssService/RssController.cs
+++ TbotRssService/RssController.cs
@@ -36,54 +36,59 @@
             new RemoveHtml(),
         };
 
-         public async Task<SyndicationFeedResult> Feed()
-         {
-             SyndicationFeed feed = await this.LoadFeed();
+        /// <summary>
+        /// Main controller method. Transform the RSS feed and serve the result.
+        /// </summary>
+        /// <returns>The transformed RSS feed.</returns>
+        [OutputCache(CacheProfile = "RssCache")]
+        public async Task<SyndicationFeedResult> Feed()
+        {
+            SyndicationFeed feed = await this.LoadFeed();
 
-             this.TransformFeed(feed);
+            this.TransformFeed(feed);
 
-             return new SyndicationFeedResult(feed);
-         }
+            return new SyndicationFeedResult(feed);
+        }
         
-         private async Task<SyndicationFeed> LoadFeed()
-         {
-             SyndicationFeed feed;
+        private async Task<SyndicationFeed> LoadFeed()
+        {
+            SyndicationFeed feed;
 
-             using (WebClient wc = new WebClient())
-             using (Stream stream = await wc.OpenReadTaskAsync(new Uri("http://businessoftech.com/podcast/feed/1")))
-             using (XmlReader reader = XmlReader.Create(stream, new XmlReaderSettings
-             {
-                 CheckCharacters = true,
-             }))
-             {
-                 feed = SyndicationFeed.Load(reader);
-             }
+            using (WebClient wc = new WebClient())
+            using (Stream stream = await wc.OpenReadTaskAsync(new Uri("http://businessoftech.com/podcast/feed/1")))
+            using (XmlReader reader = XmlReader.Create(stream, new XmlReaderSettings
+            {
+                CheckCharacters = true,
+            }))
+            {
+                feed = SyndicationFeed.Load(reader);
+            }
 
-             feed.AttributeExtensions.Add(new XmlQualifiedName("itunes", "http://www.w3.org/2000/xmlns/"), Constants.ItunesNS.NamespaceName);
+            feed.AttributeExtensions.Add(new XmlQualifiedName("itunes", "http://www.w3.org/2000/xmlns/"), Constants.ItunesNS.NamespaceName);
 
-             return feed;
-         }
+            return feed;
+        }
 
-         private void TransformFeed(SyndicationFeed feed)
-         {
-             SyndicationVisitorContext context = new SyndicationVisitorContext
-             {
-                RssUrl = new Uri(this.Url.Action("Feed", "Rss", null, this.Request.Url.Scheme)),
-                Config = (TbotSection)WebConfigurationManager.GetSection("tbotSection"),
-             };
+        private void TransformFeed(SyndicationFeed feed)
+        {
+            SyndicationVisitorContext context = new SyndicationVisitorContext
+            {
+            RssUrl = new Uri(this.Url.Action("Feed", "Rss", null, this.Request.Url.Scheme)),
+            Config = (TbotSection)WebConfigurationManager.GetSection("tbotSection"),
+            };
 
-             foreach (ISyndicationFeedVisitor feedVisitor in RssController.FeedVisitors)
-             {
-                 feedVisitor.TransformFeed(feed, context);
-             }
+            foreach (ISyndicationFeedVisitor feedVisitor in RssController.FeedVisitors)
+            {
+                feedVisitor.TransformFeed(feed, context);
+            }
 
-             foreach (var item in feed.Items)
-             {
-                 foreach (ISyndicationItemVisitor itemVisitor in RssController.ItemVisitors)
-                 {
-                     itemVisitor.TransformItem(item, context);
-                 }
-             }
-         }
+            foreach (var item in feed.Items)
+            {
+                foreach (ISyndicationItemVisitor itemVisitor in RssController.ItemVisitors)
+                {
+                    itemVisitor.TransformItem(item, context);
+                }
+            }
+        }
     }
 }
